cdd921a217cca366ff7bd03861bec788ef89594c
annatruong <annatruong321@hotmail.com>
2019-12-23 15:51:11 +0000

    removed unused images, changed alignment and added new project

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
+    document.getElementById("image").src = element.src;
+    document.getElementById("modal-image").style.display = "block";
   }
